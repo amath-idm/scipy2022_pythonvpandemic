946d44ef3ff0eb54d315c39b9826350a3a44f8f6
cliffckerr <questionably@gmail.com>
2022-07-08 02:29:42 -0400

    update showcases

---
+++ paper_support/showcase.py
@@ -0,0 +1,40 @@
+# Set parameters and define random wave generator
+import numpy as np
+xmin = 0
+xmax = 10
+npts = 50
+noisevals = np.linspace(0, 1, 11)
+x = np.linspace(xmin, xmax, npts)
+
+def randwave(std):
+    np.random.seed()
+    a = np.cos(x)
+    b = np.random.randn(npts)
+    return a + b*std
+
+# Other imports
+import sciris as sc
+
+# Start timing
+sc.tic()
+
+# Create object in parallel
+output = sc.parallelize(randwave, noisevals)
+
+# Save to files
+filenames = []
+for n,noiseval in enumerate(noisevals):
+    filename = f'noise{noiseval:0.1f}.obj'
+    sc.save(filename, output[n])
+    filenames.append(filename)
+
+# Create dict from files
+data = sc.odict({filename:sc.load(filename) for filename in filenames})
+
+# Create 3D plot
+sc.surf3d(data[:])
+
+# Print elapsed time
+sc.toc()
+
+
